56a237cb01aa31070b8e6b54a32bb5a4080cca51
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2022-12-14 17:26:09 +0100

    ref: student service implementation

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -2,69 +2,41 @@ import {
   Body,
   Controller,
   Get,
-  NotFoundException,
   Delete,
   Param,
   HttpStatus,
 } from '@nestjs/common';
 import { HttpCode, Patch, Post } from '@nestjs/common/decorators';
 
+import { StudentsService } from './students.service';
+
 @Controller('students')
 export class StudentsController {
-  private students = [
-    {
-      id: 1,
-      name: 'Ismael',
-      lastname: 'Gonzalez',
-    },
-  ];
+  constructor(private readonly studentService: StudentsService) {}
 
   @Get()
   getStudents() {
-    return this.students;
+    return this.studentService.getStudents();
   }
 
   @Get(':studentId')
   getStudent(@Param('studentId') id: string) {
-    const student = this.students.find((u) => u.id === +id);
-
-    if (!student) {
-      throw new NotFoundException('User not found');
-    }
-
-    return student;
+    return this.studentService.getStudent(id);
   }
 
   @Post()
   createStudent(@Body() student: any) {
-    this.students.push(student);
+    return this.studentService.createStudent(student);
   }
 
   @Delete(':studentId')
   @HttpCode(HttpStatus.NO_CONTENT)
   removeUser(@Param('studentId') id: string) {
-    const index = this.students.findIndex((u) => u.id === +id);
-
-    if (index === -1) {
-      throw new NotFoundException('User not found');
-    }
-
-    this.students = this.students.slice(0, index);
+    return this.studentService.removeUser(id);
   }
 
   @Patch(':studentId')
   updateStudent(@Param('studentId') id: string, @Body() properties: any) {
-    const index = this.students.findIndex((u) => u.id === +id);
-
-    if (index === -1) {
-      throw new NotFoundException('User not found');
-    }
-
-    this.students[index] = {
-      ...this.students[index],
-      ...properties,
-    };
-
-    return { success: true };
+    return this.studentService.updateStudent(id, properties);
   }
 }
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class StudentsService {}
+export class StudentsService {
+  private students = [
+    {
+      id: 1,
+      name: 'Ismael',
+      lastname: 'Gonzalez',
+    },
+  ];
+
+  getStudents() {
+    return this.students;
+  }
+
+  getStudent(id: string) {
+    const student = this.students.find((u) => u.id === +id);
+
+    if (!student) {
+      throw new NotFoundException('Student not found');
+    }
+
+    return student;
+  }
+
+  createStudent(student: any) {
+    this.students.push(student);
+  }
+
+  removeUser(id: string) {
+    const index = this.students.findIndex((u) => u.id === +id);
+
+    if (index === -1) {
+      throw new NotFoundException('User not found');
+    }
+
+    this.students = this.students.slice(0, index);
+  }
+
+  updateStudent(id: string, properties: any) {
+    const index = this.students.findIndex((u) => u.id === +id);
+
+    if (index === -1) {
+      throw new NotFoundException('User not found');
+    }
+
+    this.students[index] = {
+      ...this.students[index],
+      ...properties,
+    };
+
+    return { success: true };
+  }
+}
