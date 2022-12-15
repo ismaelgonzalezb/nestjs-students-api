da22653a0c72cdfa7c76ff304f8f2d205a0a3cf4
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2022-12-15 18:18:54 +0100

    imp: typeorm with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [StudentsModule],
+  imports: [
+    StudentsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5435,
+      username: 'admin',
+      password: 'admin',
+      database: 'students',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
